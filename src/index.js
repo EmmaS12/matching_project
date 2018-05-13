786533e5f494304ec73e1bd4ac707ae183bd9851
Emma Saccone <emma@saccone.org>
2018-05-13 01:06:26 -0400

    cofirst project commit

---
+++ src/index.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+class App extends React.Component{
+  constructor(props ){
+    super(props);
+    this.state = {
+      order: [0,4,7,6,2,3,1,5],
+      won: false
+    }
+  }
+  hasWon(){
+  this.setState({
+      order: [0,4,7,6,2,3,1,5],
+      won: true
+    })
+  }
+  render(){
+    let text = "you haven't won"
+    if (this.state.won){
+      text = "you win"
+    }
+    return (
+      <div class="game">
+        <Board
+          order={this.state.order}
+          onClick={i => this.hasWon()}
+        />
+        <div>
+        {text}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component{
+  constructor(props ){
+    super(props);
+    this.state = {
+      order: this.props.order,
+      cur_card: 0
+    }
+  }
+
+  handleclick(i){
+    if (i === this.state.cur_card) {
+      this.state.cur_card += 1
+      if (this.state.cur_card === 8) {
+        this.props.onClick()
+      }
+    } else {
+      alert("wrong")
+      this.state.cur_card = 0
+    }
+
+  }
+  render(){
+
+    return (
+    <div>
+      <Card
+        order_num={this.props.order[0]}
+        onClick={i => this.handleclick(this.props.order[0])}
+      />
+      <Card
+      order_num={this.props.order[1]}
+      onClick={i => this.handleclick(this.props.order[1])}
+      />
+      <Card
+      order_num={this.props.order[2]}
+      onClick={i => this.handleclick(this.props.order[2])}
+      />
+      <Card
+      order_num={this.props.order[3]}
+      onClick={i => this.handleclick(this.props.order[3])}
+      />
+      <Card
+      order_num={this.props.order[4]}
+      onClick={i => this.handleclick(this.props.order[4])}
+      />
+      <Card
+      order_num={this.props.order[5]}
+      onClick={i => this.handleclick(this.props.order[5])}
+      />
+      <Card
+      order_num={this.props.order[6]}
+      onClick={i => this.handleclick(this.props.order[6])}
+      />
+      <Card
+      order_num={this.props.order[7]}
+      onClick={i => this.handleclick(this.props.order[7])}
+      />
+    </div>
+    );
+  }
+}
+
+class Card extends React.Component{
+  constructor(props ){
+    super(props);
+  }
+  handleClick(){
+    this.props.onClick(this.props.order_num)
+  }
+  render(){
+
+    return (
+        <button onClick={this.handleClick.bind(this)}class="card">Card</button>
+    );
+  }
+}
+
+
+
+render(<App />, document.getElementById('root'))
